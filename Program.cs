df6a87ba954f59038ad4db9e59f12fa3f4bdc9b1
mariam matared <mariammatared2@gmail.com>
2026-06-09 10:07:22 +0200

    Assignment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿namespace Advanced_2
 { 
+
     using System;
     using System.Collections.Generic;
     namespace ShopMaster
+
     {
       
         // Starter Code: Data Models & Product Catalog
